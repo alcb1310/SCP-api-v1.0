278eb9a52a78e5c097851168d5d3f65b1bba19ab
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-04-23 19:34:44 -0500

    Create Version20220424003418.php
    Created the migration for the entities partida and user

---
+++ migrations/Version20220424003418.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220424003418 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE partida (id INT AUTO_INCREMENT NOT NULL, padre_id INT DEFAULT NULL, codigo VARCHAR(50) NOT NULL, nombre VARCHAR(255) NOT NULL, acumula TINYINT(1) NOT NULL, nivel INT NOT NULL, INDEX IDX_A9C1580C613CEC58 (padre_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nombre VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE partida ADD CONSTRAINT FK_A9C1580C613CEC58 FOREIGN KEY (padre_id) REFERENCES partida (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE partida DROP FOREIGN KEY FK_A9C1580C613CEC58');
+        $this->addSql('DROP TABLE partida');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
